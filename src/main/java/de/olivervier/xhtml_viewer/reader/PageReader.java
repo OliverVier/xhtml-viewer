2747c879cdda1aafad2e168c1bab08a478de7694
Oliver Vier <119545999+OliverVier@users.noreply.github.com>
2024-09-22 20:46:50 +0200

    filter xhtml pages

---
+++ src/main/java/de/olivervier/xhtml_viewer/reader/PageReader.java
@@ -0,0 +1,73 @@
+package de.olivervier.xhtml_viewer.reader;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageReader {
+
+	/**
+	 * Read xhtml-files in given filepath
+	 * @param dir
+	 */
+	public List<File> filterPages(String ...dirPath) {
+		
+		if(dirPath == null) {
+			System.err.println("No paths given");
+			return null;
+		}
+		
+		List<File> directories = new ArrayList<>();
+		for(String fp : dirPath) {
+			directories.add(new File(fp));
+		}
+
+		List<File> files = new ArrayList<File>();
+		for(File dir : directories) {			
+			for(File file : dir.listFiles()) {
+				if(file.getName().lastIndexOf(".xhtml") != -1) {				
+					files.add(file);
+				}
+			}
+		}
+		return files;		
+	}
+	
+	
+	/**
+	 * Read xhtml-files in given filepath recursively
+	 * @param dir
+	 */
+	public List<File> filterPagesRecursive(String ...dirPath) {
+		if(dirPath == null) {
+			System.err.println("No paths given");
+			return null;
+		}
+		
+		List<File> directories = new ArrayList<>();
+		for(String fp : dirPath) {
+			directories.addAll(readDirectoriesRec(fp));
+		}
+
+		List<File> files = new ArrayList<File>();
+		for(File dir : directories) {			
+			for(File file : dir.listFiles()) {
+				if(file.getName().lastIndexOf(".xhtml") != -1) {				
+					files.add(file);
+				}
+			}
+		}
+		return files;
+	}
+	
+	public List<File> readDirectoriesRec(String dirPath) {
+		List<File> dirs = new ArrayList<>();
+		dirs.add(new File(dirPath));
+		for(File file : new File(dirPath).listFiles()) {
+			if(file.isDirectory()) {
+				dirs.addAll(readDirectoriesRec(file.getAbsolutePath()));
+			}
+		}
+		return dirs;
+	}	
+}
