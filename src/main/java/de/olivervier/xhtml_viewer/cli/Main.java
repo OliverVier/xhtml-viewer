58c27bbc63f24ede8073b56af9bd9d818c8bbcc3
Oliver Vier <119545999+OliverVier@users.noreply.github.com>
2024-10-08 23:52:47 +0200

    test input in main

--- src/main/java/de/olivervier/xhtml_viewer/cli/Main.java
+++ src/main/java/de/olivervier/xhtml_viewer/cli/Main.java
@@ -2,6 +2,16 @@ package de.olivervier.xhtml_viewer.cli;
 
 public class Main {
 	public static void main(String[] args) {
-		new CLI().run();
+
+		//Just for testing
+		args = new String[]{"src", "true"};
+
+		String[] filepaths = new String[args.length - 1];
+		for(int i = 0; i < args.length-1; i++) {
+			filepaths[i] = args[i];
+		}
+		boolean recursive = args[args.length-1].equals("-r");
+
+		new CLI().run(filepaths, recursive);
 	}
 }
