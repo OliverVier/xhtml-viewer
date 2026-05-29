a024b0180c1e6b42c430f5b9171a085efde57410
OliverVier <119545999+OliverVier@users.noreply.github.com>
2026-05-30 00:31:28 +0200

    removed comment

--- src/main/java/de/olivervier/xhtml_viewer/Main.java
+++ src/main/java/de/olivervier/xhtml_viewer/Main.java
@@ -4,10 +4,6 @@ import de.olivervier.xhtml_viewer.cli.CLI;
 
 public class Main {
 	public static void main(String[] args) {
-
-		//basepath
-		//basepath ...relative_path
-
 		try {
 			new CLI().run(args[0]);
 		} catch (Exception e) {
