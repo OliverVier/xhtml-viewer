10251cf25448a747f5fd4a156ab82808f74917c5
OliverVier <119545999+OliverVier@users.noreply.github.com>
2026-05-30 00:45:10 +0200

    refactoring

--- src/main/java/de/olivervier/xhtml_viewer/Main.java
+++ src/main/java/de/olivervier/xhtml_viewer/Main.java
@@ -3,11 +3,16 @@ package de.olivervier.xhtml_viewer;
 import de.olivervier.xhtml_viewer.cli.CLI;
 
 public class Main {
+
 	public static void main(String[] args) {
-		try {
-			new CLI().run(args[0]);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+		new Main().run(args);
+	}
+
+	public void run(String[] args) {
+		if(args.length == 0) {
+			System.err.println("Program needs a directory path to operate");
+			return;
 		}
+		new CLI().run(args[0]);
 	}
 }
